fcb8a88df3119d913de589f1fb9353b405b46b71
shadowctrl <shadowctrl@shadowctrl.com>
2024-08-30 19:02:54 +0530

    setup init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,34 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import {
+  Inter,
+  Poppins,
+  Nunito,
+  Josefin_Sans,
+  Bebas_Neue,
+} from "next/font/google";
+import "@/styles/globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "400", "500", "600", "800"],
+  variable: "--font-poppins",
+});
+const nunito = Nunito({
+  subsets: ["latin"],
+  weight: ["200", "400", "500", "600", "800"],
+  variable: "--font-nunito",
+});
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["200", "400", "500", "600", "700"],
+  variable: "--font-josefin",
+});
+const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-bebas",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +42,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} ${poppins.variable} ${nunito.variable} ${josefin.variable} ${bebas.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
