ee2998f02a6bbbc13db8a53d2ecd1e364282c727
Shadowctrl <shadowctrl@outlook.com>
2024-09-06 22:16:39 +0530

    TagTopic Comp

---
+++ src/components/tagTopic/TagTopic.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+import "./tagTopic.scss";
+import { NextPage } from "next";
+
+interface Props {
+  topic?: string;
+  date: ReactNode;
+}
+
+const FormatDate: React.FC<DateProps> = ({ date }) => {
+  let givenDate = new Date(date);
+  let currentDate = new Date();
+  let differenceInMillis = currentDate.getTime() - givenDate.getTime();
+  let differenceInHours: number = Math.floor(
+    differenceInMillis / (1000 * 60 * 60)
+  );
+
+  return <span>{differenceInHours} hours ago</span>;
+};
+const TagTopic: NextPage<Props> = ({ topic = "Cybersecurity News", date }) => {
+  return (
+    <p className="tag-topic">
+      {topic} <span>·</span>
+      <FormatDate date={date} />
+    </p>
+  );
+};
+
+export default TagTopic;
